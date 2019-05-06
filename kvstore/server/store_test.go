9673d8bb36cc637e7e8112f1b1f7882c651af95d
LzGustavo <xavier.luizgustavo@gmail.com>
2019-05-06 18:24:38 -0300

    fix test file

--- kvstore/server/store_test.go
+++ kvstore/server/store_test.go
@@ -7,25 +7,7 @@ import (
 	"time"
 )
 
-// Test_StoreOpen tests that the store can be opened.
-func Test_StoreOpen(t *testing.T) {
-	s := New(false)
-	tmpDir, _ := ioutil.TempDir("", "store_test")
-	defer os.RemoveAll(tmpDir)
-
-	s.RaftBind = "127.0.0.1:0"
-	s.RaftDir = tmpDir
-	if s == nil {
-		t.Fatalf("failed to create store")
-	}
-
-	if err := s.StartRaft(false, "node0", s.RaftBind); err != nil {
-		t.Fatalf("failed to open store: %s", err)
-	}
-}
-
-// Test_StoreOpenSingleNode tests that a command can be applied to the log
-func Test_StoreOpenSingleNode(t *testing.T) {
+func TestCreate(t *testing.T) {
 	s := New(false)
 	tmpDir, _ := ioutil.TempDir("", "store_test")
 	defer os.RemoveAll(tmpDir)
@@ -39,36 +21,9 @@ func Test_StoreOpenSingleNode(t *testing.T) {
 	if err := s.StartRaft(true, "node0", s.RaftBind); err != nil {
 		t.Fatalf("failed to open store: %s", err)
 	}
-
-	// Simple way to ensure there is a leader.
-	time.Sleep(3 * time.Second)
-
-	if err := s.Propose("set-foo-bar"); err != nil {
-		t.Fatalf("failed to set key: %s", err.Error())
-	}
-
-	// Wait for committed log entry to be applied.
-	time.Sleep(500 * time.Millisecond)
-	value := s.Get("foo")
-	if value != "bar" {
-		t.Fatalf("key has wrong value: %s", value)
-	}
-
-	if err := s.Propose("delete-foo"); err != nil {
-		t.Fatalf("failed to delete key: %s", err.Error())
-	}
-
-	// Wait for committed log entry to be applied.
-	time.Sleep(500 * time.Millisecond)
-	value = s.Get("foo")
-	if value != "" {
-		t.Fatalf("key has wrong value: %s", value)
-	}
 }
 
-// Test_StoreInMemOpenSingleNode tests that a command can be applied to the log
-// stored in RAM.
-func Test_StoreInMemOpenSingleNode(t *testing.T) {
+func TestOperations(t *testing.T) {
 	s := New(true)
 	tmpDir, _ := ioutil.TempDir("", "store_test")
 	defer os.RemoveAll(tmpDir)
