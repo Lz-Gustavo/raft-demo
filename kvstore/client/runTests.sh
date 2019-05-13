b855699a5093e6bbb14e5cc9a2c9a910116265d1
LzGustavo <xavier.luizgustavo@gmail.com>
2019-05-13 11:21:32 -0300

    new autoexperiment routines

---
+++ kvstore/client/runTests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]
+then
+	echo "usage: $0 numberClients reqForeachClient diffHashKeys"
+	exit 1
+fi
+
+go test -v -count 1 -clients=${1} -req=${2} -key=${3} >> result.txt
--- kvstore/client/seqClient_test.go
+++ kvstore/client/seqClient_test.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"sync"
 	"testing"
+	"time"
 )
 
-func TestRequisitionsKvstore(b *testing.T) {
-	numClients := 3
-	numMessages := 300
+type config struct {
+	configFile  *string
+	numKey      int
+	numClients  int
+	numMessages int
+}
 
-	numKey := 100
+var Cfg *config
+
+func init() {
+	Cfg = new(config)
+	flag.IntVar(&Cfg.numClients, "clients", 0, "Set the number of clients")
+	flag.IntVar(&Cfg.numMessages, "req", 0, "Set the number of sent requisitions by each client")
+	flag.IntVar(&Cfg.numKey, "key", 0, "Set the number of differente keys for hash set")
+	Cfg.configFile = flag.String("testfile", "", "test config toml file")
+}
+
+func TestKvstore(b *testing.T) {
+
+	flag.Parse()
+	if Cfg.numClients == 0 || Cfg.numMessages == 0 || Cfg.numKey == 0 {
+		b.Fatal("Must define a number of clients/messages/diff keys > zero")
+	}
+	numClients := Cfg.numClients
+	numMessages := Cfg.numMessages
+
+	numKey := Cfg.numKey
 	storeValue := "-----"
+
+	measureThroughput := 50
 
 	configBarrier := new(sync.WaitGroup)
 	configBarrier.Add(numClients)
@@ -42,17 +68,42 @@ func TestRequisitionsKvstore(b *testing.T) {
 				b.Fatalf("failed to start UDP socket: %s", err.Error())
 			}
 
+			// Control variables
+			var op, coinThroughtput int
+			var start time.Time
+			var finish time.Duration
+			var flagStopwatch bool
+
 			// Wait until all goroutines finish configuration
 			configBarrier.Done()
 			configBarrier.Wait()
 
 			for k := 0; k < numMessages; k++ {
-				clients[j].Broadcast(fmt.Sprintf("set-%d-%s\n", rand.Intn(numKey), storeValue))
-				//clients[j].Broadcast(fmt.Sprintf("get-%d\n", rand.Intn(numKey)))
-				//clients[j].Broadcast(fmt.Sprintf("delete-%d\n", rand.Intn(numKey)))
 
-				repply, _ := clients[j].ReadUDP()
-				b.Log("Received value:", repply)
+				coinThroughtput = rand.Intn(measureThroughput)
+				if coinThroughtput == 0 {
+					start = time.Now()
+					flagStopwatch = true
+				}
+				op = rand.Intn(3)
+				switch op {
+				case 0:
+					clients[j].Broadcast(fmt.Sprintf("set-%d-%s\n", rand.Intn(numKey), storeValue))
+				case 1:
+					clients[j].Broadcast(fmt.Sprintf("get-%d\n", rand.Intn(numKey)))
+				case 2:
+					clients[j].Broadcast(fmt.Sprintf("delete-%d\n", rand.Intn(numKey)))
+				}
+
+				repply, err := clients[j].ReadUDP()
+				if err != nil {
+					b.Logf("UDP error: %q, caught repply: %s", err.Error(), repply)
+				}
+				if flagStopwatch {
+					finish = time.Since(start)
+					b.Log(finish)
+					flagStopwatch = false
+				}
 			}
 			finishedBarrier.Done()
 		}(i)
