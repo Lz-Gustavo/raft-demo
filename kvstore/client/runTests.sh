865b142b1f795b02a4e43535b9f29f5df9188d91
LzGustavo <xavier.luizgustavo@gmail.com>
2019-05-15 11:00:16 -0300

    running timeout experiments

--- kvstore/client/runTests.sh
+++ kvstore/client/runTests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]
+if [[ $# -ne 4 ]]
 then
-	echo "usage: $0 numberClients reqForeachClient diffHashKeys"
+	echo "usage with 4 arguments: $0 'mode(0: numMessages; 1: execTime)' 'numberClients' '(reqForeachClient||execTime)' 'diffHashKeys'"
 	exit 1
 fi
 
-#go test -v -count 1 -clients=${1} -req=${2} -key=${3} >> result.txt
-go test -count 1 -clients=${1} -req=${2} -key=${3}
+go clean -testcache
+if [ $1 -eq "0" ]; then
+	go test -run TestNumMessagesKvstore -count 1 -clients=${2} -req=${3} -key=${4}
+
+elif [ $1 -eq "1" ]; then
+	go test -run TestClientTimeKvstore -count 1 -clients=${2} -time=${3} -key=${4}
+fi
--- kvstore/client/seqClient_test.go
+++ kvstore/client/seqClient_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -15,6 +16,7 @@ type config struct {
 	numKey      int
 	numClients  int
 	numMessages int
+	execTime    int64
 }
 
 var Cfg *config
@@ -24,38 +26,34 @@ func init() {
 	flag.IntVar(&Cfg.numClients, "clients", 0, "Set the number of clients")
 	flag.IntVar(&Cfg.numMessages, "req", 0, "Set the number of sent requisitions by each client")
 	flag.IntVar(&Cfg.numKey, "key", 0, "Set the number of differente keys for hash set")
+	flag.Int64Var(&Cfg.execTime, "time", 0, "Set the execution time of the experiment")
 }
 
-func TestKvstore(b *testing.T) {
+func TestNumMessagesKvstore(b *testing.T) {
 
 	flag.Parse()
 	if Cfg.numClients == 0 || Cfg.numMessages == 0 || Cfg.numKey == 0 {
 		b.Fatal("Must define a number of clients/messages/diff keys > zero")
 	}
-	numClients := Cfg.numClients
-	numMessages := Cfg.numMessages
-
-	numKey := Cfg.numKey
 	storeValue := "-----"
-
 	measureThroughput := 50
 
 	configBarrier := new(sync.WaitGroup)
-	configBarrier.Add(numClients)
+	configBarrier.Add(Cfg.numClients)
 
 	finishedBarrier := new(sync.WaitGroup)
-	finishedBarrier.Add(numClients)
+	finishedBarrier.Add(Cfg.numClients)
 
-	outFile, err := os.OpenFile("testLatency.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	outFile, err := os.OpenFile(strconv.Itoa(Cfg.numClients)+"c-latency.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		b.Fatalf("could not open log file: %s\n", err.Error())
 	}
 	defer outFile.Close()
 	logger := log.New(outFile, "", 0)
 
-	clients := make([]*Info, numClients, numClients)
+	clients := make([]*Info, Cfg.numClients, Cfg.numClients)
 
-	for i := 0; i < numClients; i++ {
+	for i := 0; i < Cfg.numClients; i++ {
 		go func(j int) {
 
 			var err error
@@ -85,7 +83,7 @@ func TestKvstore(b *testing.T) {
 			configBarrier.Done()
 			configBarrier.Wait()
 
-			for k := 0; k < numMessages; k++ {
+			for k := 0; k < Cfg.numMessages; k++ {
 
 				op = rand.Intn(3)
 				coinThroughtput = rand.Intn(measureThroughput)
@@ -96,11 +94,11 @@ func TestKvstore(b *testing.T) {
 
 				switch op {
 				case 0:
-					clients[j].Broadcast(fmt.Sprintf("set-%d-%s\n", rand.Intn(numKey), storeValue))
+					clients[j].Broadcast(fmt.Sprintf("set-%d-%s\n", rand.Intn(Cfg.numKey), storeValue))
 				case 1:
-					clients[j].Broadcast(fmt.Sprintf("get-%d\n", rand.Intn(numKey)))
+					clients[j].Broadcast(fmt.Sprintf("get-%d\n", rand.Intn(Cfg.numKey)))
 				case 2:
-					clients[j].Broadcast(fmt.Sprintf("delete-%d\n", rand.Intn(numKey)))
+					clients[j].Broadcast(fmt.Sprintf("delete-%d\n", rand.Intn(Cfg.numKey)))
 				}
 
 				repply, err := clients[j].ReadUDP()
@@ -122,4 +120,128 @@ func TestKvstore(b *testing.T) {
 	for _, v := range clients {
 		v.Shutdown()
 	}
+}
+
+func TestClientTimeKvstore(b *testing.T) {
+
+	flag.Parse()
+	if Cfg.numClients == 0 || Cfg.execTime == 0 || Cfg.numKey == 0 {
+		b.Fatal("Must define a number of clients/execTime/diff keys > zero")
+	}
+	measureThroughput := 50
+
+	configBarrier := new(sync.WaitGroup)
+	configBarrier.Add(Cfg.numClients)
+
+	finishedBarrier := new(sync.WaitGroup)
+	finishedBarrier.Add(Cfg.numClients)
+
+	outFile, err := os.OpenFile(strconv.Itoa(Cfg.numClients)+"c-latency.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		b.Fatalf("could not open log file: %s\n", err.Error())
+	}
+	defer outFile.Close()
+	logger := log.New(outFile, "", 0)
+
+	clients := make([]*Info, Cfg.numClients, Cfg.numClients)
+	signal := make(chan bool)
+	requests := make(chan string, Cfg.numMessages)
+
+	storeValue := "-----"
+	go generateRequests(requests, signal, Cfg.numKey, storeValue)
+	go killWorkers(Cfg.execTime, signal)
+
+	for i := 0; i < Cfg.numClients; i++ {
+		go func(j int, requests chan string, kill chan bool) {
+
+			var err error
+			clients[j], err = New("client-config.toml")
+			if err != nil {
+				b.Fatalf("failed to find config: %s", err.Error())
+			}
+
+			err = clients[j].Connect()
+			if err != nil {
+				b.Fatalf("failed to connect to cluster: %s", err.Error())
+			}
+
+			clients[j].Udpport = 15000 + j
+			err = clients[j].StartUDP()
+			if err != nil {
+				b.Fatalf("failed to start UDP socket: %s", err.Error())
+			}
+
+			// Control variables
+			var coinThroughtput int
+			var start time.Time
+			var finish int64
+			var flagStopwatch bool
+
+			// Wait until all goroutines finish configuration
+			configBarrier.Done()
+			configBarrier.Wait()
+
+			for {
+				msg, ok := <-requests
+				if !ok {
+					finishedBarrier.Done()
+					return
+				}
+
+				coinThroughtput = rand.Intn(measureThroughput)
+				if coinThroughtput == 0 {
+					flagStopwatch = true
+					start = time.Now()
+				}
+
+				clients[j].Broadcast(msg)
+				repply, err := clients[j].ReadUDP()
+				if err != nil {
+					b.Logf("UDP error: %q, caught repply: %s", err.Error(), repply)
+				}
+				if flagStopwatch {
+					finish = int64(time.Since(start) / time.Nanosecond)
+					b.Log(finish)
+					logger.Println(finish)
+					flagStopwatch = false
+				}
+			}
+
+		}(i, requests, signal)
+	}
+	finishedBarrier.Wait()
+
+	for _, v := range clients {
+		v.Shutdown()
+	}
+}
+
+func generateRequests(reqs chan<- string, signal <-chan bool, numKey int, storeValue string) {
+	for {
+		var msg string
+		op := rand.Intn(3)
+
+		switch op {
+		case 0:
+			msg = fmt.Sprintf("set-%d-%s\n", rand.Intn(numKey), storeValue)
+		case 1:
+			msg = fmt.Sprintf("get-%d\n", rand.Intn(numKey))
+		case 2:
+			msg = fmt.Sprintf("delete-%d\n", rand.Intn(numKey))
+		}
+
+		select {
+		case reqs <- msg:
+			// ...
+		case <-signal:
+			close(reqs)
+			return
+		}
+	}
+}
+
+func killWorkers(timeSeconds int64, signal chan<- bool) {
+	t := time.NewTimer(time.Duration(timeSeconds) * time.Second)
+	<-t.C
+	signal <- true
 }
